5b850b30fd7c9348cf811877b554b05464b55d7c
SUHANDI <53260744+suhandiproject@users.noreply.github.com>
2019-11-20 22:54:17 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+#!usr/bin/python
+#use-python
+
+true = 'bilangan tersebut adalah Positif'
+
+false = 'bilangan tersebut adalah Negatif'
+
+leng = 'bilangan tersebut Nol'
+
+def cek(num):
+
+	global num	if num > 0:
+
+		print true
+
+		ing()
+
+	else:
+
+		if num == 0:
+
+			print leng
+
+			ing()
+
+		else:
+
+			print false
+
+			ing()
+
+def ing():
+
+	global num
+
+	num = raw_input('Masukan Bilangan : ')
+
+	cek(num)
